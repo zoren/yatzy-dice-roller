ea078c7e2ec7b53b88ea6fd0bdf360d39f91e648
Søren Sjørup <zorren@gmail.com>
2015-11-25 20:27:42 +0100

    initial working roll

--- Yatzy/Roller.elm
+++ Yatzy/Roller.elm
@@ -1,36 +1,48 @@
 module Yatzy.Roller where
 
-import Graphics.Element exposing (show)
+import Graphics.Element exposing (..)
 import Random
+import Keyboard
+
+type Action = Roll
 
 -- Main
 
-{--
-main = show <| randomSide initialSeed
---}
+main = Signal.map view model
 
 -- Model
-
+initialModel = ([1,1,1,1,1], initialSeed)
 
 -- View
+view1 : Int -> Element
+view1 eyes =
+  let
+    src = "../assets/" ++ (toString eyes) ++ ".png"
+  in
+    image 50 50 src
 
+view (eyeList, _) =
+  flow right (List.map view1 eyeList)
 
 -- Update
-
+update action (eyeList, seed) =
+  case action of
+    Roll -> List.foldl (\x (al, s) ->
+                    let (eye, s') = randomSide s in (eye :: al, s'))
+                    ([], seed)
+                    eyeList
 
 -- Signals
-
-
+inputs = Signal.map (\_ -> Roll) <| Keyboard.isDown 32
+model =
+  Signal.foldp update initialModel inputs
 -- Ports
 
--- Get an external input to use as seed for Random generator.
--- See the index.html for how that is done
-port jsSeed : Int
 
 initialSeed : Random.Seed
-initialSeed = Random.initialSeed jsSeed
+initialSeed = Random.initialSeed 1
 
 -- Remember this is a pure function, same input seed yields the same output (Int, Seed)
 randomSide : Random.Seed -> (Int, Random.Seed)
-randomSide seed = 
+randomSide seed =
   Random.generate (Random.int 1 6) seed
